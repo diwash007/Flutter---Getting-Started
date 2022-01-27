328d7547dd77f3ea624d06749800953fd3e062d2
Diwash <diwashdahal75@gmail.com>
2022-01-27 20:36:53 +0545

    http request to API

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,21 +8,37 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Location location = Location();
   @override
   void initState() {
     super.initState();
-    getLocation();
   }
 
   void getLocation() async {
-    Location location = Location();
     await location.getCurrentLocation();
     print(location.longitude);
     print(location.latitude);
   }
 
+  void getData() async {
+    double lat = location.latitude;
+    double lon = location.longitude;
+    String APIkey = '77d643c30f60000047e65116bee158af';
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$APIkey');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getLocation();
+    getData();
     return Scaffold();
   }
 }
