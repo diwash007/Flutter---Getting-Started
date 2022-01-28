6b25acf4cdfbc3b52d4c67ca80dd77fde0991ce3
Diwash <diwashdahal75@gmail.com>
2022-01-28 10:25:54 +0545

    add loading animation

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
+import '../services/networking.dart';
 import '../services/location.dart';
-import 'package:http/http.dart' as http;
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,36 +12,39 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   Location location = Location();
+  double lon;
+  double lat;
   @override
   void initState() {
     super.initState();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latitude);
-  }
-
-  void getData() async {
     double lat = location.latitude;
     double lon = location.longitude;
     String APIkey = '77d643c30f60000047e65116bee158af';
-    var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$APIkey');
-    http.Response response = await http.get(url);
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-    } else {
-      print(response.statusCode);
-    }
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$APIkey');
+
+    var weatherData = networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    getLocation();
-    getData();
-    return Scaffold();
+    getLocationData();
+    return Scaffold(
+      body: Center(
+        child: SpinKitSpinningLines(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
