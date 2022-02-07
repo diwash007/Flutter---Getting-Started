c2e4b99321aba63e593cc3f7430ee06193c3ef8b
Diwash <diwashdahal75@gmail.com>
2022-02-07 12:41:22 +0545

    add spinner on login and register

--- flash_chat/lib/screens/login_screen.dart
+++ flash_chat/lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import '../components/rounded_button.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool showSpinner = false;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
@@ -21,61 +23,65 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              controller: emailController,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: textFieldDecoration,
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              controller: passwordController,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration:
-                  textFieldDecoration.copyWith(hintText: 'Enter your password'),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            Hero(
-              tag: 'login',
-              child: RoundedButton(
-                color: Colors.lightBlueAccent,
-                title: 'Log In',
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                        email: emailController.text,
-                        password: passwordController.text);
-                    Navigator.pushNamed(context, ChatScreen.path);
-                  } catch (e) {
-                    print(e);
-                  }
-                },
+              const SizedBox(
+                height: 48.0,
               ),
-            ),
-          ],
+              TextField(
+                controller: emailController,
+                decoration: textFieldDecoration,
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: textFieldDecoration.copyWith(
+                    hintText: 'Enter your password'),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Hero(
+                tag: 'login',
+                child: RoundedButton(
+                  color: Colors.lightBlueAccent,
+                  title: 'Log In',
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text);
+                      Navigator.pushNamed(context, ChatScreen.path);
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- flash_chat/lib/screens/registration_screen.dart
+++ flash_chat/lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import '../components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
@@ -14,6 +15,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  bool showSpinner = false;
   late String email;
   late String password;
 
@@ -21,61 +23,71 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: textFieldDecoration,
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
                 textAlign: TextAlign.center,
-                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
                 onChanged: (value) {
-                  password = value;
+                  email = value;
                 },
-                decoration: textFieldDecoration.copyWith(
-                    hintText: 'Enter your password')),
-            const SizedBox(
-              height: 24.0,
-            ),
-            Hero(
-              tag: 'register',
-              child: RoundedButton(
-                color: Colors.blueAccent,
-                title: 'Register',
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    Navigator.pushNamed(context, ChatScreen.path);
-                  } catch (e) {
-                    print(e);
-                  }
-                },
+                decoration: textFieldDecoration,
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: textFieldDecoration.copyWith(
+                      hintText: 'Enter your password')),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Hero(
+                tag: 'register',
+                child: RoundedButton(
+                  color: Colors.blueAccent,
+                  title: 'Register',
+                  onPressed: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      Navigator.pushNamed(context, ChatScreen.path);
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
