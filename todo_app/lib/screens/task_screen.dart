5819d49b8669658646e718fe915e7109f5463add
Diwash <diwashdahal75@gmail.com>
2022-02-08 20:21:42 +0545

    add container and fab

--- todo_app/lib/screens/task_screen.dart
+++ todo_app/lib/screens/task_screen.dart
@@ -8,35 +8,62 @@ class TaskScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(
-            30.0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              CircleAvatar(
-                child: Icon(
-                  Icons.list,
-                  size: 30.0,
-                ),
-                backgroundColor: Colors.white,
-                radius: 30.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(
+                30.0,
               ),
-              SizedBox(
-                height: 10.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    child: Icon(
+                      Icons.list,
+                      size: 30.0,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const Text(
+                    'TodoApp',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const Text(
+                    '12 tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                'TodoApp',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w700,
-                ),
+            ),
+            Flexible(
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    )),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(Icons.add),
       ),
     );
   }
